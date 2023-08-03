daf9afcae578058b2d4b9731562dbd406ede6834
ihordodukh <ihor.dodukh@p2h.com>
2023-08-03 13:04:22 +0100

    remove extra qase annotations

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,22 +3,10 @@ from qaseio.pytest import qase
 
 class TestClass:
 
-    @qase.suite("Authorization")
     @qase.id(1)
-    @qase.title("Authorization")
-    @qase.fields(
-        ("priority", "hight"),
-        ("layer", "e2e")
-    )
     def test_authorization(self):
         assert 1 == 1, ""
 
-    @qase.suite("Authorization")
     @qase.id(2)
-    @qase.title("Sign up")
-    @qase.fields(
-        ("priority", "hight"),
-        ("layer", "e2e")
-    )
     def test_sign_up(self):
         assert 1 != 1, "Assertion failed to to unexpected value"
