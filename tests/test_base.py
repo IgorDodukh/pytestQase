946fc4ae09c3a16eff42cf89dc2f010507265f88
ihordodukh <ihor.dodukh@p2h.com>
2023-08-03 13:34:33 +0100

    try to add steps

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,7 +7,14 @@ class TestClass:
     @qase.id(1)
     @qase.title("Authorization")
     def test_authorization(self):
-        assert 1 == 1, ""
+        with qase.step("Go to sign up page https://qase.io/login"):
+            print("complete step 1")
+        with qase.step('Fill the form with login "test" and password "test"'):
+            print("complete step 2")
+        with qase.step('Check "remember me" checkbox'):
+            print("complete step 3")
+        with qase.step('Click on the "Login" button'):
+            assert 1 == 1, ""
 
     @qase.suite("Authorization")
     @qase.id(2)
