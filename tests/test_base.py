611bc684c956a696334f0fde07b0ee2c7fe34c64
ihordodukh <ihor.dodukh@p2h.com>
2023-08-03 13:09:35 +0100

    add basic TC description

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,10 +3,14 @@ from qaseio.pytest import qase
 
 class TestClass:
 
+    @qase.suite("Authorization")
     @qase.id(1)
+    @qase.title("Authorization")
     def test_authorization(self):
         assert 1 == 1, ""
 
+    @qase.suite("Authorization")
     @qase.id(2)
+    @qase.title("Sign up")
     def test_sign_up(self):
         assert 1 != 1, "Assertion failed to to unexpected value"
