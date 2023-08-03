ee10b7284934bbaa3f9743b291826c6bf15251de
ihordodukh <ihor.dodukh@p2h.com>
2023-08-03 13:00:35 +0100

    update qase annotations

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,22 +3,22 @@ from qaseio.pytest import qase
 
 class TestClass:
 
+    @qase.suite("Authorization")
     @qase.id(1)
     @qase.title("Authorization")
     @qase.fields(
-        ("severity", "blocker"),
         ("priority", "hight"),
-        ("layer", "e2e"),
+        ("layer", "E2E"),
     )
     def test_authorization(self):
         assert 1 == 1, ""
 
+    @qase.suite("Authorization")
     @qase.id(2)
     @qase.title("Sign up")
     @qase.fields(
-        ("severity", "blocker"),
         ("priority", "hight"),
-        ("layer", "e2e"),
+        ("layer", "E2E"),
     )
     def test_sign_up(self):
         assert 1 != 1, "Assertion failed to to unexpected value"
